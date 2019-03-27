dd5ce15c1be9dbd8ce70b03e5b593e94f6238cbd
Natalia Cancino Rojas <ncancino@opticenter.cl>
2019-03-27 09:28:51 -0300

    actualización 2019

---
+++ js/modals.js
@@ -0,0 +1,29 @@
+
+
+$('#modalTrigger1').click(function() {
+  $('#modal_adaLovelace').plainModal('open');
+});
+
+$('#modalTrigger2').click(function() {
+  $('#modal_cecor').plainModal('open');
+});
+
+$('#modalTrigger3').click(function() {
+    $('#modal_cloudBreak').plainModal('open');
+  });
+
+$('#modalTrigger4').click(function() {
+    $('#modal_safeTravel').plainModal('open');
+  });
+
+$('#modalTrigger5').click(function() {
+    $('#modal_sportyKids').plainModal('open');
+  });
+
+$('#modalTrigger6').click(function() {
+    $('#modal_vinay').plainModal('open');
+  });
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# This is my Personal Portfolio Webpage / Este es mi portafolio personal
+
+[`Link to de website`](https://natcancein.github.io/portafolioNataliaCancino/)
+
+Also this is the fifth responsive web design project for Free Code Camp.
+
+## The website is built with HTML5 and CSS3, Javascript, Jquery, Flexbox and CSS Grid. 
+
